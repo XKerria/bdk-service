9894f188a6ddef8bf35fe25a2244e6c28ca2dab4
xkerria <xkerria@126.com>
2021-07-01 00:08:09 +0800

    调整用户填充

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,11 +14,13 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::factory()->create([
-            'name' => '饶中浩',
-            'phone' => '18166750612'
+        User::factory()->createMany([
+            ['name' => '饶中浩', 'phone' => '18166750612'],
+            ['name' => '苏省南', 'phone' => '15985101597'],
+            ['name' => '张林鹏', 'phone' => '18685101987'],
+            ['name' => '戴继龙', 'phone' => '18984184886'],
+            ['name' => '杨华军', 'phone' => '18111111111'],
+            ['name' => '张志愿', 'phone' => '18222222222'],
         ]);
-
-        User::factory()->count(20)->create();
     }
 }
